572365fe165a0189f528919ea2d27f4a0febf2b4
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2024-07-20 09:20:13 -0300

    feat: create account controller

---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,53 @@
+import {
+  Body,
+  Controller,
+  Post,
+  ConflictException,
+  UsePipes,
+} from "@nestjs/common";
+import { PrismaService } from "@prisma/prisma.service";
+import { ZodValidationPipe } from "../pipes/zod-validation-pipe";
+
+import { hash } from "bcryptjs";
+import { z } from "zod";
+
+const createAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+});
+
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>;
+
+@Controller("/accounts")
+@UsePipes(new ZodValidationPipe(createAccountBodySchema))
+export class CreateAccountController {
+  constructor(private prisma: PrismaService) {}
+
+  @Post()
+  async execute(@Body() body: CreateAccountBodySchema) {
+    const { name, email, password } = createAccountBodySchema.parse(body);
+
+    const userWithSameEmail = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (userWithSameEmail) {
+      throw new ConflictException(
+        "User with same e-mail address already exists."
+      );
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+  }
+}
