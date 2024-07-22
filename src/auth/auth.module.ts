4462f57b7135ae6b268e041bd916a393de6a9e78
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2024-07-22 08:19:01 -0300

    feat: auth controller

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,31 @@
+import { Module } from "@nestjs/common";
+import { PassportModule } from "@nestjs/passport";
+import { JwtModule } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
+import { Env } from "src/env";
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      global: true,
+      useFactory(config: ConfigService<Env, true>) {
+        const privateKey = config
+          .get("JWT_PRIVATE_KEY", { infer: true })
+          .trim();
+        const publicKey = config.get("JWT_PUBLIC_KEY", { infer: true }).trim();
+
+        return {
+          signOptions: { algorithm: "RS256" },
+          privateKey: Buffer.from(privateKey, "base64"),
+          publicKey: Buffer.from(publicKey, "base64"),
+        };
+      },
+    }),
+  ],
+  providers: [],
+  controllers: [],
+  exports: [],
+})
+export class AuthModule {}
