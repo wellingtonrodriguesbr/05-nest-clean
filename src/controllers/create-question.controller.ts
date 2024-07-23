deac44bbf997181d33b7ab03d92953a4157bdab2
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2024-07-23 06:52:22 -0300

    feat: create question controller

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,56 +1,51 @@
-import {
-  Body,
-  Controller,
-  Post,
-  ConflictException,
-  UsePipes,
-  UseGuards,
-} from "@nestjs/common";
+import { Controller, Post, UseGuards, Body } from "@nestjs/common";
 import { PrismaService } from "@prisma/prisma.service";
 import { ZodValidationPipe } from "../pipes/zod-validation-pipe";
 import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
-
-import { hash } from "bcryptjs";
+import { CurrentUser } from "src/auth/current-user-decorator";
+import { UserPayload } from "src/auth/jwt.strategy";
 import { z } from "zod";
 
 const createQuestionBodySchema = z.object({
-  name: z.string(),
-  email: z.string().email(),
-  password: z.string(),
+  title: z.string(),
+  content: z.string(),
 });
 
 type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>;
 
+const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema);
+
 @Controller("/questions")
 @UseGuards(JwtAuthGuard)
-@UsePipes(new ZodValidationPipe(createQuestionBodySchema))
 export class CreateQuestionController {
   constructor(private prisma: PrismaService) {}
 
   @Post()
-  async execute(@Body() body: CreateQuestionBodySchema) {
-    const { name, email, password } = createQuestionBodySchema.parse(body);
+  async execute(
+    @Body(bodyValidationPipe) body: CreateQuestionBodySchema,
+    @CurrentUser() user: UserPayload
+  ) {
+    const { title, content } = createQuestionBodySchema.parse(body);
 
-    const userWithSameEmail = await this.prisma.user.findUnique({
-      where: {
-        email,
-      },
-    });
+    const authorId = user.sub;
+    const slug = this.convertToSlug(title);
 
-    if (userWithSameEmail) {
-      throw new ConflictException(
-        "User with same e-mail address already exists."
-      );
-    }
-
-    const hashedPassword = await hash(password, 8);
-
-    await this.prisma.user.create({
+    await this.prisma.question.create({
       data: {
-        name,
-        email,
-        password: hashedPassword,
+        title,
+        content,
+        slug,
+        authorId,
       },
     });
+  }
+
+  private convertToSlug(title: string): string {
+    return title
+      .toLowerCase()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")
+      .replace(/[^\w\s-]/g, "")
+      .replace(/\s+/g, "-");
   }
 }
