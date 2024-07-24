612a25b0bba6430330c20a1b434d8bd3a0b5e645
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2024-07-24 08:55:06 -0300

    feat: WIP setup vitest e2e tests

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,9 +1,9 @@
 import { Controller, Post, UseGuards, Body } from "@nestjs/common";
-import { PrismaService } from "@prisma/prisma.service";
+import { PrismaService } from "@/prisma/prisma.service";
 import { ZodValidationPipe } from "../pipes/zod-validation-pipe";
-import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
-import { CurrentUser } from "src/auth/current-user-decorator";
-import { UserPayload } from "src/auth/jwt.strategy";
+import { JwtAuthGuard } from "@/auth/jwt-auth.guard";
+import { CurrentUser } from "@/auth/current-user-decorator";
+import { UserPayload } from "@/auth/jwt.strategy";
 import { z } from "zod";
 
 const createQuestionBodySchema = z.object({
