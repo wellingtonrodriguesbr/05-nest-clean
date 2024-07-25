0c92aac97f4aa11e69cd4c36567c78b18c1ad9da
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2024-07-25 07:34:01 -0300

    test: test (e2e) to create account and authenticate

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
   },
   ignorePatterns: [".eslintrc.js"],
   rules: {
+    "prefer-const": "off",
     "prettier/prettier": "off",
     "@typescript-eslint/interface-name-prefix": "off",
     "@typescript-eslint/explicit-function-return-type": "off",
--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     include: ["**/*.e2e-spec.ts"],
     globals: true,
     root: "./",
-    setupFiles: ["./test/setup-e2e.rs"],
+    setupFiles: ["./test/setup-e2e.ts"],
   },
   plugins: [
     tsConfigPaths(),
