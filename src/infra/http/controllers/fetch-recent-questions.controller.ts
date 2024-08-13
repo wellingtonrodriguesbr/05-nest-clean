225605cee747d6cc58dc46054303e50e209fb294
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2024-08-13 07:13:46 -0300

    feat: question presenter

--- src/infra/http/controllers/fetch-recent-questions.controller.ts
+++ src/infra/http/controllers/fetch-recent-questions.controller.ts
@@ -3,6 +3,7 @@ import { JwtAuthGuard } from "@/infra/auth/jwt-auth.guard";
 import { ZodValidationPipe } from "@/infra/http/pipes/zod-validation-pipe";
 import { z } from "zod";
 import { FetchRecentQuestionsUseCase } from "@/domain/forum/application/use-cases/fetch-recent-questions";
+import { QuestionPresenter } from "../presenters/question-presenter";
 
 const pageQueryParamSchema = z
   .string()
@@ -23,10 +24,16 @@ export class FetchRecentQuestionsController {
   async execute(
     @Query("page", queryValidationPipe) page: PageQueryParamSchema
   ) {
-    const questions = await this.fetchRecentQuestions.execute({
+    const result = await this.fetchRecentQuestions.execute({
       page,
     });
 
-    return { questions };
+    if (result.isLeft()) {
+      throw new Error();
+    }
+
+    const questions = result.value.questions;
+
+    return { questions: questions.map(QuestionPresenter.toHTTP) };
   }
 }
