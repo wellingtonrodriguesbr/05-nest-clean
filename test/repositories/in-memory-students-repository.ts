ea8e0cd5c14a3a6237ea3ca2b521b5293ef1a2a6
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2024-08-20 06:53:14 -0300

    test: unit tests register and authenticate student

---
+++ test/repositories/in-memory-students-repository.ts
@@ -0,0 +1,22 @@
+import { DomainEvents } from "@/core/events/domain-events";
+import { StudentsRepository } from "@/domain/forum/application/repositories/students-repository";
+import { Student } from "@/domain/forum/enterprise/entities/student";
+
+export class InMemoryStudentsRepository implements StudentsRepository {
+  public items: Student[] = [];
+
+  async create(student: Student) {
+    this.items.push(student);
+    DomainEvents.dispatchEventsForAggregate(student.id);
+  }
+
+  async findByEmail(email: string) {
+    const student = this.items.find((item) => item.email === email);
+
+    if (!student) {
+      return null;
+    }
+
+    return student;
+  }
+}
