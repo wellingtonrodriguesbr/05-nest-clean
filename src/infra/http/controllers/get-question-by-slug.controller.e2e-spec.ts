c1f506c2c9d9ddf7e1d6ad74d94d58493d44dcdc
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2024-09-19 07:56:09 -0300

    test: test e2e get question by slug controller

---
+++ src/infra/http/controllers/get-question-by-slug.controller.e2e-spec.ts
@@ -0,0 +1,63 @@
+import request from "supertest";
+
+import { AppModule } from "@/infra/app.module";
+import { INestApplication } from "@nestjs/common";
+import { Test } from "@nestjs/testing";
+import { PrismaService } from "@/infra/database/prisma/prisma.service";
+import { JwtService } from "@nestjs/jwt";
+
+describe("Get question by slug (E2E)", () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+  let jwt: JwtService;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    prisma = moduleRef.get(PrismaService);
+    jwt = moduleRef.get(JwtService);
+
+    await app.init();
+  });
+
+  test("[GET] /questions/:slug", async () => {
+    const user = await prisma.user.create({
+      data: {
+        name: "John Doe",
+        email: "johndoe@example.com",
+        password: "123456",
+      },
+    });
+
+    const accessToken = jwt.sign({ sub: user.id });
+
+    await prisma.question.createMany({
+      data: [
+        {
+          title: "New question created",
+          content: "The content of question",
+          slug: "new-question-created",
+          authorId: user.id,
+        },
+        {
+          title: "New question created 2",
+          content: "The content of question",
+          slug: "new-question-created-2",
+          authorId: user.id,
+        },
+      ],
+    });
+
+    const response = await request(app.getHttpServer())
+      .get("/questions/new-question-created")
+      .set("Authorization", `Bearer ${accessToken}`);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({
+      question: expect.objectContaining({ title: "New question created" }),
+    });
+  });
+});
