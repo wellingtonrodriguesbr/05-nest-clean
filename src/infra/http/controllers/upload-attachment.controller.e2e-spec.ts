c62552c52514751d64f748a640448433ff1cafc2
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2025-05-30 08:24:47 -0300

    chore: wip upload attachment controller

---
+++ src/infra/http/controllers/upload-attachment.controller.e2e-spec.ts
@@ -0,0 +1,40 @@
+import request from "supertest";
+
+import { AppModule } from "@/infra/app.module";
+import { INestApplication } from "@nestjs/common";
+import { Test } from "@nestjs/testing";
+import { JwtService } from "@nestjs/jwt";
+import { StudentFactory } from "test/factories/make-student";
+import { DatabaseModule } from "@/infra/database/database.module";
+
+describe("Upload Attachment (E2E)", () => {
+  let app: INestApplication;
+  let jwt: JwtService;
+  let studentFactory: StudentFactory;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    studentFactory = moduleRef.get(StudentFactory);
+    jwt = moduleRef.get(JwtService);
+
+    await app.init();
+  });
+
+  test("[POST] /attachments", async () => {
+    const user = await studentFactory.makePrismaStudent();
+
+    const accessToken = jwt.sign({ sub: user.id.toString() });
+
+    const response = await request(app.getHttpServer())
+      .post("/attachments")
+      .set("Authorization", `Bearer ${accessToken}`)
+      .attach("file", "./test/e2e/sample-image-upload.jpg");
+
+    expect(response.statusCode).toBe(201);
+  });
+});
